56596f70b8daec14105ccaa0cf5dda24a2e822af
Vadim Naimushin <nvadimus@mail.ru>
2022-07-26 11:04:02 +0500

    task №1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -14,6 +14,9 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
 
+    let message = productName + " " + "за " + productPrice + " " + "теперь в корзине!";
+    console.log(message);
+
     // Конец решения задания №1.1.
 
     return message;
