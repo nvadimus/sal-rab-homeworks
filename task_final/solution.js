e58f3352baad2f7127c5caef36929bf8450093f7
Vadim Naimushin <nvadimus@mail.ru>
2022-07-27 13:03:57 +0500

    final task test 3

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i ++) {
+    for (let i = 0; i < countOfGoods; i ++) {
         data.goods.count = data.goods.push(goods[i].title);
     }
     
