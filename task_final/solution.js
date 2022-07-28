ebb763d95100a6ef0f35e08d0d3de1ed0695d994
Vadim Naimushin <nvadimus@mail.ru>
2022-07-28 17:25:14 +0500

    finat task work on mistakes 2

--- task_final/solution.js
+++ task_final/solution.js
@@ -26,18 +26,16 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: "", order: {}, goods: []};
-
-    let q = data;
+    let data = {client: "", order: {}, goods: [] };
 
     data.client = name + " " + phone;
-    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     data.order.sum = sum;
 
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i ++) {
-        data.goods.push(goods[i].title, goods[i].count);
+        data.goods.push({title:goods[i].title, count: goods[i].count});
     }
     
     let jsonData = JSON.stringify({data});
